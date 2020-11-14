f73d5179fa508207fd7abf6a1ef9592d4b4a6883
烨烨 <541767316@qq.com>
2020-11-14 15:20:30 +0800

    third

---
+++ mock/menu.js
@@ -0,0 +1,24 @@
+Mock.mock('/menu', {
+    'dataSource|18': [{
+        'key|+1': 1,
+        'titles|2-4': [{
+            name: '@cword(2,4)',
+            href: '@url("http")'
+        }],
+        'content': {
+            'tabs|2-5': [{
+                name: '@cword(2,5)',
+                href: '@url'
+            }],
+            'subs|8-15': [{
+                'activity|1': ['','每300减40','199减100'],
+                category: '@cword(2,3)',
+                href: '@url',
+                'items|8-20': [{
+                    href: '@url',
+                    name: '@cword(2,6)'
+                }]
+            }]
+        }
+    }]
+})
