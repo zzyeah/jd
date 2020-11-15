d8055afe2621b486f3c580ca6547841b23fdbdab
烨烨 <541767316@qq.com>
2020-11-15 22:53:48 +0800

    未进行整理的完整版

--- js/index.js
+++ js/index.js
@@ -1,2 +1,4 @@
 $('#fs > .w').load('fs.html');
-$('#header > .w').load('search.html')
+$('#header > .w').load('search.html');
+$('#J_seckill > .w').load('seckill.html');
+$('#shortcut > .w').load('shortcut.html');
